ba0b1ee0ce557771fc9a5b85f6440838dc924135
Krunal <kmist1@unh.newhaven.edu>
2020-10-07 18:16:16 -0400

    adding program1 of homeWork6

---
+++ homeWork6/program1.py
@@ -0,0 +1,23 @@
+# Indefinitely monitoring file existance at certain location
+# By Krunal Mistry
+# Created on 10/7/2020
+
+import os
+
+isExists = True
+
+def fileDirExists(fileName):
+    isExists = os.path.exists(fileName)
+    return os.path.exists(fileName)
+
+path = input("Please Enter Name of File, Directory or path: ")
+
+while(isExists):
+    print(fileDirExists(path))
+    isExists = fileDirExists(path)
+
+print("File does not exists")
+
+
+
+
