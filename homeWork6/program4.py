4bdde5fc646b60518a1239f2cfdc0585d0355945
Krunal <kmist1@unh.newhaven.edu>
2020-10-09 11:47:24 -0400

    update program4

--- homeWork6/program4.py
+++ homeWork6/program4.py
@@ -1,14 +1,14 @@
 import os,fnmatch
 
-start_dir = "v1"
-
 def file_finder(startPath, fileName):
     for dirpath, dirs, files in os.walk(startPath):
         for file in files:
             if (file == fileName):
                 return os.path.join(dirpath,file)
 
-print(file_finder(start_dir,'index.html'))
+start_dir = input("Please Enter Start Directory or path: ")
+fileName = input("Please Enter name of file to search: ")
+print("Path of your file is: ",file_finder(start_dir,fileName))
 
 
 
